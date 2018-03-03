3b83b8efd498a00e6280c7672207701ef9a9cb34
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-03-03 19:56:50 +0800

    Standardizer

--- simul.py
+++ simul.py
@@ -1,13 +1,30 @@
 print("==========IEEE-754 Binary-64 floating point converter==========")
 binaryChar = "10."
+stdexp = 0
 
 # CHECK IF VALID BINARY INPUT #
 def bin_check(inp):
-    if(inp.count(".") > 1):
+    if(inp.count(".") > 1 or inp[0] is "."):
         return False
     
     return all(x in binaryChar for x in inp)
-
+    
+def standardizeBin(binput):
+    global stdexp
+    stdexp = exp
+    while True:
+        if(binput[0] is "1" and binput[1] is "."):
+            return binput
+        elif(binput[0] is "0"):
+            binput = str(float(binput) * 10)
+            print(binput)
+            stdexp = stdexp+1
+        else:
+            binput = str(float(binput) * 0.1)
+            print(binput)
+            stdexp = stdexp-1
+            
+            
 while True:
     binary_input = input("Input binary number: ")
     if bin_check(binary_input):
@@ -24,3 +41,9 @@ while True:
         break
     except ValueError:
         print("ERROR: INVALID INPUT. TRY AGAIN")
+
+print("Input is " + binary_input + " x 2^" + str(exp))        
+
+stdBin = standardizeBin(binary_input)
+print("Standardized Input is " + stdBin + " x 2^" + str(stdexp))
+
