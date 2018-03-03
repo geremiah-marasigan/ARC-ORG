5b46324630f5643f30290235f72a686612c1edac
Zach <geremiah_marasigan@dlsu.edu.ph>
2018-03-03 19:03:50 +0800

    Added Input check

---
+++ simul.py
@@ -0,0 +1,22 @@
+import re
+
+print("==========IEEE-754 Binary-64 floating point converter==========")
+binaryChar = "10."
+
+# CHECK IF VALID BINARY INPUT #
+def bin_check(inp):
+    if(inp.count(".") > 1):
+        return False
+        
+    return all(x in binaryChar for x in inp)
+
+while True:
+    binary_input = input("Input binary number: ")
+    if bin_check(binary_input):
+        break
+    else:
+        print("ERROR: INVALID INPUT. TRY AGAIN")
+
+
+
+
